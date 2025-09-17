abc2c19d40665de741d634c64f69cd3ea2d8b889
kavyarajasekar <47854473+kavyarajasekar@users.noreply.github.com>
2025-09-17 18:00:56 +0530

    Update eks.tf

--- eks/eks.tf
+++ eks/eks.tf
@@ -4,7 +4,7 @@ module "eks" {
 
   name              = local.cluster_name
   kubernetes_version = "1.31"
-  iam_role_arn = aws_iam_role.eks_cluster_role.arn
+  #iam_role_arn = aws_iam_role.eks_cluster_role.arn
   
 
   vpc_id                         = module.vpc.vpc_id
@@ -28,29 +28,29 @@ module "eks" {
 
   }
 
-  /*eks_managed_node_group_defaults = {
+  eks_managed_node_group_defaults = {
     ami_type = "AL2_x86_64"
-  }*/
+  }
 
   eks_managed_node_groups = {
     one = {
       name           = "node-group-1"
-      ami_type = "AL2_x86_64"
+      #ami_type = "AL2_x86_64"
       instance_types = ["t3.small"]
       min_size       = 1
       max_size       = 3
       desired_size   = 2
-      iam_role_arn   = aws_iam_role.eks_node_group_role.arn
+      #iam_role_arn   = aws_iam_role.eks_node_group_role.arn
     }
 
     two = {
       name           = "node-group-2"
-      ami_type = "AL2_x86_64"
+     # ami_type = "AL2_x86_64"
       instance_types = ["t3.small"]
       min_size       = 1
       max_size       = 2
       desired_size   = 1
-      iam_role_arn   = aws_iam_role.eks_node_group_role.arn
+     # iam_role_arn   = aws_iam_role.eks_node_group_role.arn
       
     }
   }
